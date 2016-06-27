334870b283c9221249af23fef830e8c92d671c68
Chris Imeson <chris.imeson@insight.com>
2016-06-27 14:17:01 +0100

    updated banner

--- src/main/java/uk/co/frameworktraining/controller/HelloWorldController.java
+++ src/main/java/uk/co/frameworktraining/controller/HelloWorldController.java
@@ -4,11 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @Controller
 public class HelloWorldController {
-	@RequestMapping("/")
+	@RequestMapping(value = "/", produces = "text/plain")
 	@ResponseBody
 	String home() {
-		return "Success! The Framework Training template microservices is up and running";
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/banner.txt"), "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+                sb.append("\n");
+			}
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+		return sb.toString();
 	}
 }
